ed110a070e80ff3c82c7e1a10649b38501937cb5
PndP2357 <ssy@sosuyeob-ui-MacBookAir.local>
2024-02-12 18:12:06 +0900

    axios test

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("HEY!");
+const { default: axios } = require("axios");
+
+axios.get("https://youtube.com/").then((res) => {
+    console.log(res.data);
+    console.log(res.status);
+});
